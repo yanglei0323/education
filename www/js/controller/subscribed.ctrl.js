3f984a50b01e3d4179a6aadf13ef1d61066fb16e
trhuo <891567977@qq.com>
2016-12-19 23:42:28 +0800

    解决登录界面背景图滚动问题

--- www/js/controller/subscribed.ctrl.js
+++ www/js/controller/subscribed.ctrl.js
@@ -1,3 +1,4 @@
-educationApp.controller('subscribedCtrl', ['$scope', function ($scope) {
+educationApp.controller('subscribedCtrl', ['$scope', 'Http', function ($scope) {
 	console.log('已订阅控制器');
+
 }]);
